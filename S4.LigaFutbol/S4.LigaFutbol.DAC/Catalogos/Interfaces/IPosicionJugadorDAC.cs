f4efd68e3a3e89877425da648f3009c7ed911d9f
Jorge Adrian Barragan Perez <jabarragan@geb.mx>
2025-08-21 16:09:36 -0600

    Feat se agrego el dac de posicion de jugador

---
+++ S4.LigaFutbol/S4.LigaFutbol.DAC/Catalogos/Interfaces/IPosicionJugadorDAC.cs
@@ -0,0 +1,10 @@
+﻿namespace S4.LigaFutbol.DAC.Catalogos.Interfaces;
+
+public interface IPosicionJugadorDAC
+{
+    Task<int> InsertarPosicionesJugador(PosicionesJugador posicionesJugador, int IdUsuario);
+    Task<bool> ActualizarPosicionesJugador(PosicionesJugador posicionesJugador, int IdUsuario);
+    Task<List<PosicionesJugador>> ListaPosicionesJugador();
+    Task<PosicionesJugador> PosicionesJugador(int IdPosicionJuagdor);
+    Task<bool> EliminarPosicionesJugador(int IdPosicionJuagdor, int IdUsuario);
+}
---
+++ S4.LigaFutbol/S4.LigaFutbol.DAC/Catalogos/PosicionJugadorDAC.cs
@@ -0,0 +1,84 @@
+﻿namespace S4.LigaFutbol.DAC.Catalogos;
+
+public class PosicionJugadorDAC : IPosicionJugadorDAC
+{
+    private readonly Conexion _conexion;
+    public PosicionJugadorDAC(Conexion ConnectionString)
+    {
+        _conexion = ConnectionString;
+    }
+
+    public async Task<bool> ActualizarPosicionesJugador(PosicionesJugador posicionesJugador, int IdUsuario)
+    {
+        using (var conexion = _conexion.ObtieneConexion())
+        {
+            var parametros = new DynamicParameters();
+
+            parametros.Add("@p_IdPosicionJugador", posicionesJugador.IdPosicionJugador, DbType.Int32);
+            parametros.Add("@p_Nombre", posicionesJugador.Nombre, DbType.String);
+            parametros.Add("@p_Abreviatura", posicionesJugador.Abreviatura, DbType.String);
+
+
+            parametros.Add("@p_IdUsuario", IdUsuario, DbType.UInt32);
+            var Resultado = await conexion.ExecuteAsync("SP_POSICION_JUGADOR_ED", parametros, commandType: CommandType.StoredProcedure);
+            return Resultado > 0;
+        }
+    }
+
+    public async Task<bool> EliminarPosicionesJugador(int IdPosicionJuagdor, int IdUsuario)
+    {
+        using (var conexion = _conexion.ObtieneConexion())
+        {
+            var parametros = new DynamicParameters();
+            parametros.Add("@p_IdPosicionJugador", IdPosicionJuagdor, DbType.Int32);
+
+            parametros.Add("@p_IdUsuario", IdUsuario, DbType.UInt32);
+            var Resultado = await conexion.ExecuteAsync("SP_POSICION_JUGADOR_DEL", parametros, commandType: CommandType.StoredProcedure);
+            return Resultado > 0;
+        }
+    }
+
+    public async Task<int> InsertarPosicionesJugador(PosicionesJugador posicionesJugador, int IdUsuario)
+    {
+        using (var conexion = _conexion.ObtieneConexion())
+        {
+            var parametros = new DynamicParameters();
+            parametros.Add("@p_Nombre", posicionesJugador.Nombre, DbType.String);
+            parametros.Add("@p_Abreviatura", posicionesJugador.Abreviatura, DbType.String);
+
+            parametros.Add("@p_IdUsuario", IdUsuario, DbType.UInt32);
+            var Resultado = await conexion.ExecuteScalarAsync<int>("SP_POSICION_JUGADOR_IN", parametros, commandType: CommandType.StoredProcedure);
+            return Resultado;
+        }
+    }
+
+    public async Task<List<PosicionesJugador>> ListaPosicionesJugador()
+    {
+        List<PosicionesJugador> Lista = new List<PosicionesJugador>();
+        try
+        {
+            using (var conexion = _conexion.ObtieneConexion())
+            {
+                Lista = (await conexion.QueryAsync<PosicionesJugador>("SP_POSICIONES_JUGADOR_CT", commandType: CommandType.StoredProcedure)).ToList();
+            }
+
+            return Lista;
+        }
+        catch
+        {
+            return Lista;
+        }
+    }
+
+    public async Task<PosicionesJugador> PosicionesJugador(int IdPosicionJuagdor)
+    {
+        using (var conexion = _conexion.ObtieneConexion())
+        {
+
+            var parametros = new DynamicParameters();
+            parametros.Add("@p_IdPosicionJuagdor", IdPosicionJuagdor, DbType.Int32);
+
+            return await conexion.QueryFirstOrDefaultAsync<PosicionesJugador>("SP_POSICIONES_JUGADOR_CN", parametros, commandType: CommandType.StoredProcedure);
+        }
+    }
+}

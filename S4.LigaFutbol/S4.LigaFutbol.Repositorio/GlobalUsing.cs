80574ea54545d18388d6bbd8e3c8686825084a04
Jorge Adrian Barragan Perez <jabarragan@geb.mx>
2025-08-22 18:23:42 -0600

    Feat se agrego el interface en operacion

--- S4.LigaFutbol/S4.LigaFutbol.Repositorio/GlobalUsing.cs
+++ S4.LigaFutbol/S4.LigaFutbol.Repositorio/GlobalUsing.cs
@@ -1,16 +1,16 @@
 ﻿global using S4.LigaFutbol.DAC.Catalogos.Interfaces;
 global using S4.LigaFutbol.DAC.Estadisticas.Interfaces;
+global using S4.LigaFutbol.DAC.Operacion.Interfaces;
+
+
 
 global using S4.LigaFutbol.Repositorio.Catalogos.Interfaces;
 global using S4.LigaFutbol.Repositorio.Estadisticas.Interfaces;
-global using S4.LigaFutbol.Comunes.Operacion.OperacionDTO;
-
-
+global using S4.LigaFutbol.Repositorio.Operacion.Interfaces;
 
 
 global using S4.LigaFutbol.Comunes.Catalogos;
 global using S4.LigaFutbol.Comunes.Operacion;
-global using S4.LigaFutbol.Comunes.Estadisticas;
-
+global using S4.LigaFutbol.Comunes.Operacion.OperacionDTO;
 global using S4.LigaFutbol.Comunes.Catalogos.DTO;
 global using S4.LigaFutbol.Comunes.Estadisticas.EstadisticasDTO;
---
+++ S4.LigaFutbol/S4.LigaFutbol.Repositorio/Operacion/EquipoRepositorio.cs
@@ -0,0 +1,42 @@
+﻿namespace S4.LigaFutbol.Repositorio.Operacion;
+
+public class EquipoRepositorio : IEquipoRepositorio
+{
+    private readonly IEquipoDAC equipoDAC;
+    public async Task<Equipos> ActualizarEquipo(Equipos equipos, int IdUsuario)
+    {
+        Equipos equiposObjecto = new Equipos();
+        var Actualizo = await equipoDAC.ActualizarEquipo(equipos, IdUsuario);
+        if (Actualizo)
+            equiposObjecto = equipos;
+
+        return equiposObjecto;
+    }
+
+    public async Task<Equipos> Equipo(int IdEquipo)
+    {
+        Equipos equiposObjecto = new Equipos();
+        equiposObjecto = await equipoDAC.Equipo(IdEquipo);
+
+        return equiposObjecto;
+    }
+
+    public async Task<Equipos> InsertarEquipo(Equipos equipos, int IdUsuario)
+    {
+        Equipos equiposObjecto = new Equipos();
+
+        var IdEquipo = await equipoDAC.InsertarEquipo(equipos, IdUsuario);
+        if (IdEquipo > 0)
+            equiposObjecto = await equipoDAC.Equipo(IdEquipo);
+
+        return equiposObjecto;
+    }
+
+    public async Task<List<Equipos>> ListaEquipo()
+    {
+        List<Equipos> ListaequiposObjecto = new List<Equipos>();
+        ListaequiposObjecto = await equipoDAC.ListaEquipo();
+
+        return ListaequiposObjecto;
+    }
+}
